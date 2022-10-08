086a3f9fa650b49ea56a740804026069f85a8324
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-10-07 22:03:04 -0400

    lab1 bronx update

--- group2/lab1/lab1_bronx1.R
+++ group2/lab1/lab1_bronx1.R
@@ -1,20 +1,17 @@
 library(gdata)
-#faster xls reader but requires perl!
-bronx1<-read.xls(file.choose(),pattern="BOROUGH",stringsAsFactors=FALSE,sheet=1,perl="<SOMEWHERE>/perl/bin/perl.exe") 
-bronx1<-bronx1[which(bronx1$GROSS.SQUARE.FEET!="0" & bronx1$LAND.SQUARE.FEET!="0" & bronx1$SALE.PRICE!="$0"),]
+library(xlsx)
 
-#alternate
-#library("xlsx", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
-#bronx1<-read.xlsx("<SOMEWHERE>/rollingsales_bronx.xls",pattern="BOROUGH",stringsAsFactors=FALSE,sheetIndex=1,startRow=5,header=TRUE)
+# load data
+bronx1<-read.xlsx("rollingsales_bronx.xls",pattern="BOROUGH",stringsAsFactors=FALSE,sheetIndex=1,startRow=5,header=TRUE)
+bronx1<-bronx1[which(bronx1$GROSS.SQUARE.FEET!="0" & bronx1$LAND.SQUARE.FEET!="0" & bronx1$SALE.PRICE!="$0"),]
 View(bronx1)
-#
-attach(bronx1) # If you choose to attach, leave out the "data=." in lm regression
-SALE.PRICE<-sub("\\$","",SALE.PRICE) 
-SALE.PRICE<-as.numeric(gsub(",","", SALE.PRICE)) 
-GROSS.SQUARE.FEET<-as.numeric(gsub(",","", GROSS.SQUARE.FEET)) 
-LAND.SQUARE.FEET<-as.numeric(gsub(",","", LAND.SQUARE.FEET)) 
-plot(log(GROSS.SQUARE.FEET), log(SALE.PRICE)) 
-m1<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET))
+
+bronx1$SALE.PRICE<-sub("\\$", "", bronx1$SALE.PRICE) 
+bronx1$SALE.PRICE<-as.numeric(gsub(",","", bronx1$SALE.PRICE)) 
+bronx1$GROSS.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$GROSS.SQUARE.FEET))
+bronx1$LAND.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$LAND.SQUARE.FEET)) 
+plot(log(bronx1$GROSS.SQUARE.FEET), log(bronx1$SALE.PRICE)) 
+m1<-lm(log(bronx1$SALE.PRICE)~log(bronx1$GROSS.SQUARE.FEET))
 summary(m1)
 abline(m1,col="red",lwd=2)
 plot(resid(m1))
@@ -24,6 +21,7 @@ plot(resid(m1))
 m2<-lm(log(bronx1$SALE.PRICE)~log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
 summary(m2)
 plot(resid(m2))
+
 # Suppress intercept - using "0+ ..."
 m2a<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
 summary(m2a)
